cb8a25715c3bfcd7c7e9ac2bc25ecf225164961d
Born-by-Code <tsdrafi@gmail.com>
2022-08-25 20:44:37 +0600

    For night Office

--- js/case.js
+++ js/case.js
@@ -3,6 +3,7 @@ function UpdateCaseNumber(isIncrease){
     const CasePriveousString = CaseNumberField.value;
     const CaseNumberInt = parseInt(CasePriveousString);
 
+    
     if(isIncrease === true){
         CaseNumberupdate = CaseNumberInt + 1;
     }
@@ -11,13 +12,21 @@ function UpdateCaseNumber(isIncrease){
     }
 
     CaseNumberField.value = CaseNumberupdate;
+    return CaseNumberupdate;
 }
 
+function UpdateCaseTotalPrize(){
+    const CaseTotalPrize = document.getElementById('case-prize');
+    CaseTotalPrize.innerText = CaseNumberupdate * 59;
+}
 document.getElementById('case-btm-plus').addEventListener('click',function(){
     
-    UpdateCaseNumber(true);
+    const CaseNumberupdate = UpdateCaseNumber(true);
+    UpdateCaseTotalPrize()
 })
 document.getElementById('case-btm-minus').addEventListener('click',function(){
-    UpdateCaseNumber(false);
+    
+    const CaseNumberupdate = UpdateCaseNumber(false);
+    UpdateCaseTotalPrize()
     
 })
